7faddcd841b06a8ca6bdc9b4f2bdb6b601b366d6
Kevin Flansburg <kevin.flansburg@dominodatalab.com>
2021-06-14 15:34:40 +0000

    Use public workflow image; remove presto

--- fetch_data.py
+++ fetch_data.py
@@ -1,21 +1,5 @@
-import prestodb
-import pandas as pd
-# https://prestodb.io/docs/current/overview/concepts.html
+import urllib.request
 
-conn=prestodb.dbapi.connect(
-    host='polaris-dp-prereqs-presto',
-    port=8080,
-    user='the-user',
-    catalog='postgresql', # presto catalog = postgresql database
-    schema='public',  # presto schema = postgresql schema
-    # auth=prestodb.auth.BasicAuthentication("principal id", "password"),
-)
-
-cur = conn.cursor()
-cur.execute('SELECT * FROM postgresql.public.wines limit 10')
-rows = cur.fetchall()
-titles = [col[0] for col in cur.description]
-df = pd.DataFrame(rows, columns=titles)
-df.to_pickle("/mnt/vol/repo/artifacts/data.pkl")
-
-print(df.head())
+with urllib.request.urlopen('https://polaris-sample-workflow-data.s3.us-west-1.amazonaws.com/data.pkl') as f:
+    with open('/mnt/vol/repo/artifacts/data.pkl', 'wb') as o:
+        o.write(f.read())
